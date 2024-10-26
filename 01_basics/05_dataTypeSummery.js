a3cb3fa63013d7fa0109a87983fdb967008bdb59
amitkumar8340441436@gmail.com <amitkumar8340441436@gmail.com>
2024-10-26 19:45:13 +0530

    data type summery complete

--- 01_basics/05_dataTypeSummery.js
+++ 01_basics/05_dataTypeSummery.js
@@ -1,9 +1,60 @@
 // premitive DataType  
 // 7 types :  String, Number, Boolean, Null, Undefine, Symbol, BigInt
 
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail = undefined
+let userEmail1
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+const bigNumber = 656655555879654565554555555555n
+// console.log(bigNumber)
 
 //
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // non premitive / Reference type DataType
 // Array, Objects, Functions
+
+const heros = ["saktiman", "thor", "ironman"]
+// console.log(typeof heros)
+
+const obj1 ={
+    name : "amit",
+    age : 25,
+}
+// console.log(typeof obj1)
+
+
+
+
+const myfunction = function(){
+    console.log("Hello Amit")
+}
+
+// myfunction()
+
+console.log(typeof myfunction)
