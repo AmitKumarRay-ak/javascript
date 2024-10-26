75c46a4e48c5c76826ac6234ba69099aeabda209
amitkumar8340441436@gmail.com <amitkumar8340441436@gmail.com>
2024-10-26 16:59:49 +0530

    datatypes and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "nonar811@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountstate;
+
+
+// accountId = 2       // not changeble const variable
+accountEmail = "abc@gmail.com"
+accountPassword = "123"
+accountCity = "xyz"
+
+// console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello")       // we are using nodejs, not browser
+
+// console.log(3 + 3);
+
+
+
+
+
+
+let name = "Amit Kumar"
+let age = 25
+let isLoggedIn = true
+
+// number
+// bigInt
+// string
+// boolean => true / false
+// null => stand Alone value
+// undefine => defined but not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof(undefined))
