219333c52d8171dcf063e9ccdc322a76f2d77df0
amitkumar8340441436@gmail.com <amitkumar8340441436@gmail.com>
2024-10-26 18:49:27 +0530

    comprission

---
+++ 01_basics/04_comprision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)   // here null converted into zero
+
+
+// console.log(undefined > 0)      // false
+// console.log(undefined == 0)     // false
+// console.log(undefined >= 0)     // false
+
+
+//   ===    tripal equal check 
+
+console.log("1" === 1)
