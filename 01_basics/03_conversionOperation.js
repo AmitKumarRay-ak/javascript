140e80846cecb631d089f50456b1514b8a7d6d03
amitkumar8340441436@gmail.com <amitkumar8340441436@gmail.com>
2024-10-26 17:59:55 +0530

    Conversion Of Data Type Completed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// let someNumber = 33
+// let convertInString = String(someNumber)
+// console.log(convertInString)
+// console.log(typeof convertInString)
+
+// "33" => 33        easilly convert
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+
+
+// let isLogIn = 1            // giving true
+// let isLogIn = 0            // giving false
+// let isLogIn = ""           // giving false
+// let isLogIn = "A"          // giving true
+
+// let booleanIsLoggedIn = Boolean(isLogIn)
+// console.log(booleanIsLoggedIn)
